225fed408ae20a570fc1556fa8021c259a15bc10
zhouhao <529184430@qq.com>
2019-01-25 16:48:31 +0800

    1

---
+++ Scripts/test_001.py
@@ -0,0 +1,12 @@
+
+class Test_001:
+
+
+    def test_001_1(self):
+        print("---->test_001-1")
+        assert True
+
+    def test_001_2(self):
+        print("---->test_001-2")
+        assert False
+
